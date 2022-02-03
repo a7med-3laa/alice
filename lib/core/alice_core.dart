10f142e7708985f3f68434b5f330820eb004aad3
Ahmed Alaa <ahmed.alaa.cis@gmail.com>
2022-02-03 03:23:40 +0200

    Update alice_core.dart

--- lib/core/alice_core.dart
+++ lib/core/alice_core.dart
@@ -230,7 +230,7 @@ class AliceCore {
       "Alice (total: ${callsSubject.value.length} requests)",
       message,
       platformChannelSpecifics,
-      payload: "",
+      payload: "Alice",
     );
     _notificationMessageShown = message;
     _notificationProcessing = false;
